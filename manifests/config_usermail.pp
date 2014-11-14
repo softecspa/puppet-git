d89d18f2d00c8cfd30e39f87f7861d1d001d9f59
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-11-14 08:41:44 +0000

    aggiunta variabile ambiente home

--- manifests/config_usermail.pp
+++ manifests/config_usermail.pp
@@ -1,15 +1,17 @@
 define git::config_usermail (
   $git_username="root on ${::hostname}",
-  $git_usermail="root@${::fqdn}"
+  $git_usermail="root@${::fqdn}",
 ) {
 
   exec { 'set-git-username':
-    command => "/usr/bin/git config --global user.name '${git_username}'",
-    unless  => "/usr/bin/git config --global user.name | egrep '^${git_username}\$'",
+    command     => "/usr/bin/git config --global user.name '${git_username}'",
+    unless      => "/usr/bin/git config --global user.name | egrep '^${git_username}\$'",
+    environment => [ 'HOME=/root' ]
   }
 
   exec { 'set-git-usermail':
-    command => "/usr/bin/git config --global user.email '${git_usermail}'",
-    unless  => "/usr/bin/git config --global user.email | egrep '^${git_usermail}\$'",
+    command     => "/usr/bin/git config --global user.email '${git_usermail}'",
+    unless      => "/usr/bin/git config --global user.email | egrep '^${git_usermail}\$'",
+    environment => [ 'HOME=/root' ]
   }
 }
